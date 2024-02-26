f572f6879c245faedc54b651297db37c65114dcd
vbcintranet <156912019+vbcintranet@users.noreply.github.com>
2024-02-27 10:02:43 +1100

    Update to 1.6.3

--- customise/script.js
+++ customise/script.js
@@ -1,5 +1,5 @@
 (() => {
-const version = "v1.6.2";
+const version = "v1.6.3";
 
 const consol = {
   log: (message, title="Core", colour="#FF6961") => { console.log(`%c(${title}) %c${message}`, `color:${colour};font-weight:bold`, "") },
