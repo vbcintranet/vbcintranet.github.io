be9259c57bbe9c891d3328ed0397d0047b0a8326
vbcintranet <156912019+vbcintranet@users.noreply.github.com>
2026-03-01 21:11:40 +1100

    Update to v2.4.3

--- guides/script.js
+++ guides/script.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = "v2.4.2";
+  const version = "v2.4.3";
 
   const consol = {
     log: (message, title="Core", colour="#FF6961") => { console.log(`%c(${title}) %c${message}`, `color:${colour};font-weight:bold`, "") },
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vbc-intranet-v2.4.2';
+const CACHE_NAME = 'vbc-intranet-v2.4.3';
 
 const PRECACHE_ASSETS = [
   '/',
